81d556c7fef303ee10757b8f0e57dc1f9a0271b8
Richardson Sarabosing <ricsar012@gmail.com>
2025-12-24 01:12:58 -0500

    Update general routes to use Axios with async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,42 +1,71 @@
-const express = require('express');
-const router = express.Router();
-const books = require('./booksdb.js');
+const express = require("express");
+const axios = require("axios");
 
-// Get all books
-router.get('/books', (req, res) => {
-  res.json(books);
-});
+const public_users = express.Router();
 
-// Get book by ISBN
-router.get('/books/isbn/:isbn', (req, res) => {
-  const isbn = req.params.isbn;
-  res.json(books[isbn]);
-});
+// Change this ONLY if your backend runs on a different host/port
+const BASE_URL = "http://localhost:5000";
 
-// Get books by author
-router.get('/books/author/:author', (req, res) => {
-  const author = req.params.author;
-  let result = {};
-
-  for (let key in books) {
-    if (books[key].author === author) {
-      result[key] = books[key];
-    }
+// Helper: standard error response
+function handleAxiosError(res, err) {
+  if (err.response) {
+    // Backend replied with an error status
+    return res.status(err.response.status).json(err.response.data);
   }
-  res.json(result);
-});
+  // Network / server down / unknown
+  return res.status(500).json({ message: "Server error", error: err.message });
+}
 
-// Get books by title
-router.get('/books/title/:title', (req, res) => {
-  const title = req.params.title;
-  let result = {};
-
-  for (let key in books) {
-    if (books[key].title === title) {
-      result[key] = books[key];
-    }
+/**
+ * GET: Retrieve all books
+ */
+public_users.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/books`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return handleAxiosError(res, err);
   }
-  res.json(result);
 });
 
-module.exports.general = router;
+/**
+ * GET: Retrieve a book by ISBN
+ */
+public_users.get("/isbn/:isbn", async (req, res) => {
+  try {
+    const { isbn } = req.params;
+    const response = await axios.get(`${BASE_URL}/books/isbn/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    // If the backend returns 404, pass it through
+    return handleAxiosError(res, err);
+  }
+});
+
+/**
+ * GET: Retrieve books by author
+ */
+public_users.get("/author/:author", async (req, res) => {
+  try {
+    const { author } = req.params;
+    const response = await axios.get(`${BASE_URL}/books/author/${author}`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return handleAxiosError(res, err);
+  }
+});
+
+/**
+ * GET: Retrieve books by title
+ */
+public_users.get("/title/:title", async (req, res) => {
+  try {
+    const { title } = req.params;
+    const response = await axios.get(`${BASE_URL}/books/title/${title}`);
+    return res.status(200).json(response.data);
+  } catch (err) {
+    return handleAxiosError(res, err);
+  }
+});
+
+module.exports.general = public_users;
